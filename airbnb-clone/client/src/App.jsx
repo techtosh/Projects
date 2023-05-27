bd0ab7c3ba087dea77c0aba0296703bd0c336e82
internship311 <techtosh588@gmail.com>
2023-05-27 22:38:18 +0530

    Account Page has been made

--- airbnb-clone/client/src/App.jsx
+++ airbnb-clone/client/src/App.jsx
@@ -6,6 +6,7 @@ import { Layout } from "./Layout";
 import { RegisterPage } from "./pages/RegisterPage";
 import axios from "axios";
 import { UserContextProvider } from "./context/UserContext";
+import AccountNav from "./pages/AccountNav";
 
 axios.defaults.baseURL = "http://localhost:4000";
 axios.defaults.withCredentials = true;
@@ -18,6 +19,9 @@ const App = () => {
           <Route index element={<IndexPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
+          <Route path="/account" element={<AccountNav />} />
+          <Route path="/account/places" element={<AccountNav />} />
+          <Route path="/account/bookings" element={<AccountNav />} />
         </Route>
       </Routes>
     </UserContextProvider>
