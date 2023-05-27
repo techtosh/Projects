500cbbfdd1c2e11f4997bb2df5e0204f4cca395e
internship311 <techtosh588@gmail.com>
2023-05-27 18:33:25 +0530

    Login and Register Page has been made

---
+++ airbnb-clone/api/index.js
@@ -0,0 +1,79 @@
+const express = require("express");
+const cors = require("cors");
+const bcrypt = require("bcryptjs");
+const mongoose = require("mongoose");
+const jwt = require('jsonwebtoken');
+const User = require("./models/User.js");
+const cookieParser = require('cookie-parser');
+require("dotenv").config();
+const app = express();
+const bcryptSalt = bcrypt.genSaltSync(10);
+const jwtSecret = 'kjbfkfrkfnerknflerkfnler43fkf';
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(
+  cors({
+    credentials: true,
+    origin: "http://localhost:3000",
+  })
+);
+
+mongoose.connect(process.env.MONGO_URL);
+
+app.get("/test", (req, res) => {
+  res.json("test ok");
+});
+
+app.post("/register", async (req, res) => {
+  const { name, email, password } = req.body;
+  try {
+    const userDoc = await User.create({
+      name,
+      email,
+      password: bcrypt.hashSync(password, bcryptSalt),
+    });
+    res.json(userDoc);
+  } catch (e) {
+    res.status(422).json(e);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const userDoc = await User.findOne({ email });
+  if (userDoc) {
+    const passOk = bcrypt.compareSync(password, userDoc.password);
+    if (passOk) {
+      jwt.sign({
+        email:userDoc.email,
+        id:userDoc._id
+      }, jwtSecret, {}, (err,token) => {
+        if (err) { 
+          throw err;
+        }
+        res.cookie('token', token).json(userDoc);
+      });
+    } else {
+      res.status(422).json('incorrect password');
+    }
+  } else {
+    res.status(422).json("not found");
+  }
+});
+
+app.get('/profile', (req,res) => {
+  const {token} = req.cookies;
+  if (token) {
+    jwt.verify(token, jwtSecret, {}, async(err, userData) => {
+      if (err) throw err;
+      const {name,email,_id} = await User.findById(userData.id);
+      res.json({name,email,_id});
+    });
+  } else {
+    res.json(null);
+  }
+});
+
+app.listen(4000);
---
+++ airbnb-clone/client/src/App.jsx
@@ -0,0 +1,27 @@
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import { IndexPage } from "./pages/IndexPage";
+import { LoginPage } from "./pages/LoginPage";
+import { Layout } from "./Layout";
+import { RegisterPage } from "./pages/RegisterPage";
+import axios from "axios";
+import { UserContextProvider } from "./context/UserContext";
+
+axios.defaults.baseURL = "http://localhost:4000";
+axios.defaults.withCredentials = true;
+
+const App = () => {
+  return (
+    <UserContextProvider>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<IndexPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+        </Route>
+      </Routes>
+    </UserContextProvider>
+  );
+};
+
+export default App;
